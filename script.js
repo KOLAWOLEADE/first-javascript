bcf9773741e1980e1ed8d18366dc604ef077c238
Kolawole Adeyemo <kolawole_adeyemo@yahoo.com>
2022-05-14 16:06:39 +0100

    add a function to saySomething

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+// DEclearing a function to say Hi
 function sayHi(){
     alert("hello world from Acedu !!");
 }
+// calling my function
 sayHi();
 sayHi();
 sayHi();
+
+
+function saySomething(whatToSay){
+
+    alert(whatToSay);
+}
+
+saySomething("hello world");
+saySomething("how are you?");
+saySomething("are you a developer?");
+
+// function to add 2 numbers
+
+function add(number1, number2){
+
+    return number1 + number2
+}
+
+alert(add(3,5))
+console.log(add(3,10));
+document.write(add(4,34));
